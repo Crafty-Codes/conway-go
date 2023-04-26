016e99e058a941390cb2fe36a70e3a11f65d0e6f
Fabio Oberhofer <fabio.oberhofer@securiton.ch>
2023-04-26 10:12:03 +0200

    src: added survival logic

--- main.go
+++ main.go
@@ -8,11 +8,15 @@ import (
 func main() {
 	space := object.NewSpace(5)
 
-	print(space)
+	for i := 0; i < 5; i++ {
+		print(space)
+		fmt.Println()
 
+		space = survival(space)
+	}
 }
 
-func print(space [][]object.Block) {
+func print(space object.Space) {
 	for r := range space {
 		for c := range space[r] {
 			if space[r][c].Vitality == object.DEAD {
@@ -23,4 +27,40 @@ func print(space [][]object.Block) {
 		}
 		fmt.Println()
 	}
+}
+
+func survival(space object.Space) object.Space {
+	newSpace := object.NewSpace(len(space))
+
+	for r := range space {
+		for c := range space[r] {
+			life := getSurvivalCount(space, r, c)
+
+			if life == 3 {
+				newSpace[r][c] = object.Block{Vitality: object.ALIVE}
+			} else if space[r][c].Vitality == object.ALIVE && life == 2 {
+				newSpace[r][c] = object.Block{Vitality: object.ALIVE}
+			}
+		}
+	}
+
+	return newSpace
+}
+
+func getSurvivalCount(space object.Space, row int, column int) uint8 {
+	var counter uint8 = 0
+
+	for r := row - 1; r <= row+1; r++ {
+		for c := column - 1; c <= column+1; c++ {
+			if r != row || c != column {
+				if (r >= 0 && r < len(space)) && (c >= 0 && c < len(space)) {
+					if space[r][c].Vitality == object.ALIVE {
+						counter++
+					}
+				}
+			}
+		}
+	}
+
+	return counter
 }
